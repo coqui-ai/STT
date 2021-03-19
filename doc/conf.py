b4e8802aff5eb0dbf03233d811ab14bf6c9a0149
Reuben Morais <reuben.morais@gmail.com>
2021-03-19 10:25:42 +0100

    Switch doc theme to Furo

--- doc/conf.py
+++ doc/conf.py
@@ -74,8 +74,8 @@ extensions = [
   'sphinx.ext.intersphinx',
   'sphinx.ext.mathjax',
   'sphinx.ext.viewcode',
-  'sphinx_rtd_theme',
   'sphinx_js',
+  'sphinx_csharp',
   'breathe'
 ]
 
@@ -128,15 +128,7 @@ add_module_names = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-html_theme_options = {
-  'collapse_navigation': False,
-}
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
