4e189d9c67c7dac896383d84a26090308c4ffe2d
Reuben Morais <reuben.morais@gmail.com>
2021-03-29 21:06:38 +0200

    Rename model export metadata flags

--- training/coqui_stt_training/util/flags.py
+++ training/coqui_stt_training/util/flags.py
@@ -117,8 +117,8 @@ def create_flags():
     str_val_equals_help('export_contact_info', 'public contact information of the author. Can be an email address, or a link to a contact form, issue tracker, or discussion forum. Must provide a way to reach the model authors')
     str_val_equals_help('export_license', 'SPDX identifier of the license of the exported model. See https://spdx.org/licenses/. If the license does not have an SPDX identifier, use the license name.')
     str_val_equals_help('export_language', 'language the model was trained on - IETF BCP 47 language tag including at least language, script and region subtags. E.g. "en-Latn-UK" or "de-Latn-DE" or "cmn-Hans-CN". Include as much info as you can without loss of precision. For example, if a model is trained on Scottish English, include the variant subtag: "en-Latn-GB-Scotland".')
-    str_val_equals_help('export_min_ds_version', 'minimum Coqui STT version (inclusive) the exported model is compatible with')
-    str_val_equals_help('export_max_ds_version', 'maximum Coqui STT version (inclusive) the exported model is compatible with')
+    str_val_equals_help('export_min_stt_version', 'minimum Coqui STT version (inclusive) the exported model is compatible with')
+    str_val_equals_help('export_max_stt_version', 'maximum Coqui STT version (inclusive) the exported model is compatible with')
     str_val_equals_help('export_description', 'Freeform description of the model being exported. Markdown accepted. You can also leave this flag unchanged and edit the generated .md file directly. Useful things to describe are demographic and acoustic characteristics of the data used to train the model, any architectural changes, names of public datasets that were used when applicable, hyperparameters used for training, evaluation results on standard benchmark datasets, etc.')
 
     # Reporting
