59297e526c5748393f05311f812a2f5557271677
Reuben Morais <reuben.morais@gmail.com>
2021-03-30 12:33:32 +0200

    Windows base CI

--- ci_scripts/tf-vars.sh
+++ ci_scripts/tf-vars.sh
@@ -40,9 +40,8 @@ elif [ "${OS}" = "${CI_MSYS_VERSION}" ]; then
     export CI_ARTIFACTS_DIR="$(cygpath ${CI_ARTIFACTS_DIR})"
 
     export DS_ROOT_TASK=${CI_TASK_DIR}
-    export BAZEL_VC='C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC'
-    export BAZEL_SH='C:\builds\tc-workdir\msys64\usr\bin\bash'
-    export TC_WIN_BUILD_PATH='C:\builds\tc-workdir\msys64\usr\bin;C:\Python36'
+    export BAZEL_VC="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC"
+    export BAZEL_VC_FULL_VERSION="14.28.29910"
     export MSYS2_ARG_CONV_EXCL='//'
 
     mkdir -p ${CI_TASK_DIR}/tmp/
