d45149b02e5972217749e250b0915e8b9b5d38c0
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2021-04-09 18:49:29 +0200

    NodeJS repackaging

--- ci_scripts/electronjs-tests-prod.sh
+++ ci_scripts/electronjs-tests-prod.sh
@@ -9,9 +9,9 @@ source $(dirname "$0")/asserts.sh
 bitrate=$1
 set_ldc_sample_filename "${bitrate}"
 
-model_source=${STT_PROD_MODEL}
+model_source=${DEEPSPEECH_PROD_MODEL}
 model_name=$(basename "${model_source}")
-model_source_mmap=${STT_PROD_MODEL_MMAP}
+model_source_mmap=${DEEPSPEECH_PROD_MODEL_MMAP}
 model_name_mmap=$(basename "${model_source_mmap}")
 
 download_model_prod
@@ -21,10 +21,17 @@ download_data
 node --version
 npm --version
 
+symlink_electron
+
 export_node_bin_path
 
-check_runtime_nodejs
+which electron
+which node
+
+node --version
+
+deepspeech --version
+
+check_runtime_electronjs
 
 run_prod_inference_tests "${bitrate}"
-
-run_js_streaming_prod_inference_tests "${bitrate}"
--- ci_scripts/electronjs_tflite-tests-prod.sh
+++ ci_scripts/electronjs_tflite-tests-prod.sh
@@ -9,9 +9,9 @@ source $(dirname "$0")/asserts.sh
 bitrate=$1
 set_ldc_sample_filename "${bitrate}"
 
-model_source=${STT_PROD_MODEL//.pb/.tflite}
+model_source=${DEEPSPEECH_PROD_MODEL//.pb/.tflite}
 model_name=$(basename "${model_source}")
-model_source_mmap=${STT_PROD_MODEL_MMAP//.pbmm/.tflite}
+model_source_mmap=${DEEPSPEECH_PROD_MODEL_MMAP//.pbmm/.tflite}
 model_name_mmap=$(basename "${model_source}")
 
 download_model_prod
@@ -21,10 +21,17 @@ download_data
 node --version
 npm --version
 
+symlink_electron
+
 export_node_bin_path
 
-check_runtime_nodejs
+which electron
+which node
+
+node --version
+
+deepspeech --version
+
+check_runtime_electronjs
 
 run_prodtflite_inference_tests "${bitrate}"
-
-run_js_streaming_prodtflite_inference_tests "${bitrate}"
