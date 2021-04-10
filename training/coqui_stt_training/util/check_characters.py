mozilla/DeepSpeech

Fix documentation for check_characters.py script

training/deepspeech_training/util/check_characters.py
@@ -1,8 +1,11 @@
 """
-Usage: $ python3 check_characters.py "INFILE"
- e.g.  $ python3 check_characters.py -csv /home/data/french.csv
- e.g.  $ python3 check_characters.py -csv ../train.csv,../test.csv
- e.g.  $ python3 check_characters.py -alpha -csv ../train.csv
+Usage:
+ From within the training/ directory, call this script as a module:
+
+       $ python3 -m deepspeech_training.util.check_characters "INFILE"
+ e.g.  $ python3 -m deepspeech_training.util.check_characters -csv /home/data/french.csv
+ e.g.  $ python3 -m deepspeech_training.util.check_characters -csv ../train.csv,../test.csv
+ e.g.  $ python3 -m deepspeech_training.util.check_characters -alpha -csv ../train.csv
 
 Point this script to your transcripts, and it returns
 to the terminal the unique set of characters in those

coqui-ai/STT

training/coqui_stt_training/util/check_characters.py
@@ -1,8 +1,11 @@
 """
-Usage: $ python3 check_characters.py "INFILE"
- e.g.  $ python3 check_characters.py -csv /home/data/french.csv
- e.g.  $ python3 check_characters.py -csv ../train.csv,../test.csv
- e.g.  $ python3 check_characters.py -alpha -csv ../train.csv
+Usage:
+ From within the training/ directory, call this script as a module:
+
+       $ python3 -m coqui_stt_training.util.check_characters "INFILE"
+ e.g.  $ python3 -m coqui_stt_training.util.check_characters -csv /home/data/french.csv
+ e.g.  $ python3 -m coqui_stt_training.util.check_characters -csv ../train.csv,../test.csv
+ e.g.  $ python3 -m coqui_stt_training.util.check_characters -alpha -csv ../train.csv
 
 Point this script to your transcripts, and it returns
 to the terminal the unique set of characters in those