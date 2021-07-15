a29db11a62063a97cb4950cd01a9dd55b58e4422
Josh Meyer <joshua.richard.meyer@gmail.com>
2021-07-15 06:26:06 -0400

    Change DS to STT in top python scripts

--- train.py
+++ train.py
@@ -4,9 +4,9 @@ from __future__ import absolute_import, division, print_function
 
 if __name__ == "__main__":
     try:
-        from coqui_stt_training import train as ds_train
+        from coqui_stt_training import train as stt_train
     except ImportError:
         print("Training package is not installed. See training documentation.")
         raise
 
-    ds_train.main()
+    stt_train.main()
