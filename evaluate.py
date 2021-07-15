1be3471dd8f6b4f737020bad8033c3168b9895ea
Josh Meyer <joshua.richard.meyer@gmail.com>
2021-07-15 06:26:06 -0400

    Change DS to STT in top python scripts

--- evaluate.py
+++ evaluate.py
@@ -4,9 +4,9 @@ from __future__ import absolute_import, division, print_function
 
 if __name__ == "__main__":
     try:
-        from coqui_stt_training import evaluate as ds_evaluate
+        from coqui_stt_training import evaluate as stt_evaluate
     except ImportError:
         print("Training package is not installed. See training documentation.")
         raise
 
-    ds_evaluate.main()
+    stt_evaluate.main()
