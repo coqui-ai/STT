61cc312f114839bd4898f49b2da9d5719008974b
Josh Meyer <joshua.richard.meyer@gmail.com>
2021-07-20 09:50:47 -0400

    Add required alphabet path to CI tests

--- bin/run-ci-ldc93s1_new_sdb.sh
+++ bin/run-ci-ldc93s1_new_sdb.sh
@@ -23,7 +23,7 @@ fi;
 # and when trying to run on multiple devices (like GPUs), this will break
 export CUDA_VISIBLE_DEVICES=0
 
-python -u train.py --show_progressbar false --early_stop false \
+python -u train.py --alphabet_config_path "data/alphabet.txt" --show_progressbar false --early_stop false \
   --train_files ${ldc93s1_sdb} --train_batch_size 1 \
   --dev_files ${ldc93s1_sdb} --dev_batch_size 1 \
   --test_files ${ldc93s1_sdb} --test_batch_size 1 \
