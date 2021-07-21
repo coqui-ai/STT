3ee2c1bb1f6b3816d8ac6303b1ad057192e5243a
Josh Meyer <joshua.richard.meyer@gmail.com>
2021-07-21 12:09:43 -0400

    Cleaner lines for CI args

--- bin/run-ldc93s1.sh
+++ bin/run-ldc93s1.sh
@@ -20,7 +20,8 @@ fi
 # and when trying to run on multiple devices (like GPUs), this will break
 export CUDA_VISIBLE_DEVICES=0
 
-python -u train.py --alphabet_config_path "data/alphabet.txt" --show_progressbar false \
+python -u train.py --alphabet_config_path "data/alphabet.txt" \
+  --show_progressbar false \
   --train_files data/ldc93s1/ldc93s1.csv \
   --test_files data/ldc93s1/ldc93s1.csv \
   --train_batch_size 1 \
