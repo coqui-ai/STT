6ca8f5d96994b484cbe61cbb218ec3134583995b
Aya Jafari <ajafari@coqui.ai>
2021-12-15 16:20:54 +0200

    added github actions workflow for testing evaluate_export.py

---
+++ ci_scripts/evaluate-export-test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -xe
+
+source $(dirname "$0")/all-vars.sh
+source $(dirname "$0")/all-utils.sh
+
+samplerate=$1
+decoder_src=$2
+if [ "$decoder_src" != "--pypi" ]; then
+    # Use decoder package built in this CI group
+    export DS_NODECODER=1
+fi
+
+mkdir -p /tmp/train || true
+mkdir -p /tmp/train_tflite || true
+
+set -o pipefail
+python -m pip install --upgrade pip setuptools wheel | cat
+python -m pip install --upgrade . | cat
+set +o pipefail
+
+# Prepare correct arguments for training
+case "${samplerate}" in
+    8k)
+        sample_rate=8000
+        sample_name='LDC93S1_pcms16le_1_8000.wav'
+    ;;
+    16k)
+        sample_rate=16000
+        sample_name='LDC93S1_pcms16le_1_16000.wav'
+    ;;
+esac
+
+# Easier to rename to that we can exercize the LDC93S1 importer code to
+# generate the CSV file.
+echo "Moving ${sample_name} to LDC93S1.wav"
+cp "data/smoke_test/${sample_name}" "data/smoke_test/LDC93S1.wav"
+
+python bin/import_ldc93s1.py data/smoke_test
+python -m coqui_stt_training.evaluate_export  --model $STT_TEST_MODEL --csv "data/smoke_test/ldc93s1.csv" --dump /tmp/result 
+cat /tmp/result.out 
