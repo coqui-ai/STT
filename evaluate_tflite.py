6a01519e3178491b6ef316a14600e4bd420690e9
Aya Jafari <ajafari@coqui.ai>
2021-12-16 16:38:49 +0200

    fix forwarding script

--- evaluate_tflite.py
+++ evaluate_tflite.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function
 
 if __name__ == "__main__":
+
+    print(
+        "Using the top level evaluate_tflite.py script is deprecated and will be removed "
+        "in a future release. Instead use: python -m coqui_stt_training.evaluate_export"
+    )
     try:
-        from coqui_stt_training import evaluate_tflite as stt_evaluate_tflite
+        from coqui_stt_training import evaluate_export as stt_evaluate_export
     except ImportError:
         print("Training package is not installed. See training documentation.")
         raise
 
-    stt_evaluate_tflite.main()
+    stt_evaluate_export.main()
