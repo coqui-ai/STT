3b169c9108f6d933f52deff733d7f3be400d4e21
wasertech <danny@waser.tech>
2022-01-09 04:43:53 +0100

    and != or

--- bin/import_ccpmf.py
+++ bin/import_ccpmf.py
@@ -526,7 +526,7 @@ def one_sample(sample):
     elif label is None:
         # Excluding samples that failed on label validation
         _counter["invalid_label"] += 1
-    elif int(frames / SAMPLE_RATE * 1000 / 10 / 2) < len(str(label)) and file_size / len(str(label)) > 1400:
+    elif int(frames / SAMPLE_RATE * 1000 / 10 / 2) < len(str(label)) or file_size / len(str(label)) > 1400:
         # Excluding samples that are too short to fit the transcript
         _counter["too_short"] += 1
     elif frames / SAMPLE_RATE < MIN_SECS:
