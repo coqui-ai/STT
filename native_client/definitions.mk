67ab9410c24a5fc8106f3e068cc36bf76a52e823
Reuben Morais <reuben.morais@gmail.com>
2022-02-24 12:17:03 +0000

    Clean-up macOS cross-build, link against arm64 SoX

--- native_client/definitions.mk
+++ native_client/definitions.mk
@@ -34,6 +34,9 @@ ifeq ($(OS),Linux)
 LINK_STT := $(LINK_STT) -llzma -lbz2
 SOX_LDFLAGS     := -L$(ROOT_DIR)/sox-build/lib -lsox
 else ifeq ($(OS),Darwin)
+CFLAGS                  := -mmacosx-version-min=10.10 -target x86_64-apple-macos10.10
+LDFLAGS                 := -mmacosx-version-min=10.10 -target x86_64-apple-macos10.10
+
 SOX_CFLAGS              := $(shell pkg-config --cflags sox)
 LIBSOX_PATH             := $(shell echo `pkg-config --libs-only-L sox | sed -e 's/^-L//'`/lib`pkg-config --libs-only-l sox | sed -e 's/^-l//'`.dylib)
 LIBOPUSFILE_PATH        := $(shell echo `pkg-config --libs-only-L opusfile | sed -e 's/^-L//'`/lib`pkg-config --libs-only-l opusfile | sed -e 's/^-l//'`.dylib)
@@ -118,6 +121,18 @@ CFLAGS          := -target arm64-apple-ios -isysroot $(shell xcrun -sdk iphoneos
 SOX_CFLAGS      :=
 SOX_LDFLAGS     :=
 LDFLAGS         :=
+endif
+
+ifeq ($(TARGET),darwin-arm64)
+CFLAGS                  := -mmacosx-version-min=11.0 -target arm64-apple-macos11
+LDFLAGS                 := -mmacosx-version-min=11.0 -target arm64-apple-macos11
+
+SOX_CFLAGS              := $(shell arm-pkg-config --cflags sox)
+LIBSOX_PATH             := $(shell echo `arm-pkg-config --libs-only-L sox | sed -e 's/^-L//'`/lib`arm-pkg-config --libs-only-l sox | sed -e 's/^-l//'`.dylib)
+LIBOPUSFILE_PATH        := $(shell echo `arm-pkg-config --libs-only-L opusfile | sed -e 's/^-L//'`/lib`arm-pkg-config --libs-only-l opusfile | sed -e 's/^-l//'`.dylib)
+LIBSOX_STATIC_DEPS      := $(shell echo `otool -L $(LIBSOX_PATH) | tail -n +2 | cut -d' ' -f1 | grep /opt/ | sed -E "s/\.[[:digit:]]+\.dylib/\.a/" | tr '\n' ' '`)
+LIBOPUSFILE_STATIC_DEPS := $(shell echo `otool -L $(LIBOPUSFILE_PATH) | tail -n +2 | cut -d' ' -f1 | grep /opt/ | sed -E "s/\.[[:digit:]]+\.dylib/\.a/" | tr '\n' ' '`)
+SOX_LDFLAGS             := $(LIBSOX_STATIC_DEPS) $(LIBOPUSFILE_STATIC_DEPS) -framework CoreAudio -lz
 endif
 
 # -Wl,--no-as-needed is required to force linker not to evict libs it thinks we
