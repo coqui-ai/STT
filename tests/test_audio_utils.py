3884d7a663357220831fc9490ed838f73aa7087e
Josh Meyer <josh@coqui.ai>
2022-03-30 22:43:23 +0000

    cast posixpath as string

--- tests/test_audio_utils.py
+++ tests/test_audio_utils.py
@@ -14,12 +14,12 @@ class TestValidateReadAudio(unittest.TestCase):
     def test_flac(self):
         audio_path = from_here("../data/smoke_test/LDC93S1.flac")
         audio_type = audio.get_loadable_audio_type_from_extension(audio_path.suffix)
-        result = audio.read_audio(audio_type, audio_path)
+        result = audio.read_audio(audio_type, str(audio_path))
         self.assertEqual(isinstance(result[0], audio.AudioFormat))
     def test_wav(self):
         audio_path = from_here("../data/smoke_test/LDC93S1.wav")
         audio_type = audio.get_loadable_audio_type_from_extension(audio_path.suffix)
-        result = audio.read_audio(audio_type, audio_path)
+        result = audio.read_audio(audio_type, str(audio_path))
         self.assertEqual(isinstance(result[0], audio.AudioFormat))
 
 
@@ -27,12 +27,12 @@ class TestValidateReadDuration(unittest.TestCase):
     def test_flac(self):
         audio_path = from_here("../data/smoke_test/LDC93S1.flac")
         audio_type = audio.get_loadable_audio_type_from_extension(audio_path.suffix)
-        result = audio.read_duration(audio_type, audio_path)
+        result = audio.read_duration(audio_type, str(audio_path))
         self.assertEqual(isinstance(result, float))
     def test_wav(self):
         audio_path = from_here("../data/smoke_test/LDC93S1.wav")
         audio_type = audio.get_loadable_audio_type_from_extension(audio_path.suffix)
-        result = audio.read_duration(audio_type, audio_path)
+        result = audio.read_duration(audio_type, str(audio_path))
         self.assertEqual(isinstance(result, float))
 
 
