4aaee12692568af561126ed8879b86fd3390af95
Josh Meyer <josh@coqui.ai>
2022-03-30 22:38:49 +0000

    Fix import error

--- tests/test_audio_utils.py
+++ tests/test_audio_utils.py
@@ -13,12 +13,12 @@ def from_here(path):
 class TestValidateReadAudio(unittest.TestCase):
     def test_flac(self):
         audio_path = from_here("../data/smoke_test/LDC93S1.flac")
-        audio_type = get_loadable_audio_type_from_extension(audio_path.suffix)
+        audio_type = audio.get_loadable_audio_type_from_extension(audio_path.suffix)
         result = audio.read_audio(audio_type, audio_path)
         self.assertEqual(isinstance(result[0], audio.AudioFormat))
     def test_wav(self):
         audio_path = from_here("../data/smoke_test/LDC93S1.wav")
-        audio_type = get_loadable_audio_type_from_extension(audio_path.suffix)
+        audio_type = audio.get_loadable_audio_type_from_extension(audio_path.suffix)
         result = audio.read_audio(audio_type, audio_path)
         self.assertEqual(isinstance(result[0], audio.AudioFormat))
 
@@ -26,12 +26,12 @@ class TestValidateReadAudio(unittest.TestCase):
 class TestValidateReadDuration(unittest.TestCase):
     def test_flac(self):
         audio_path = from_here("../data/smoke_test/LDC93S1.flac")
-        audio_type = get_loadable_audio_type_from_extension(audio_path.suffix)
+        audio_type = audio.get_loadable_audio_type_from_extension(audio_path.suffix)
         result = audio.read_duration(audio_type, audio_path)
         self.assertEqual(isinstance(result, float))
     def test_wav(self):
         audio_path = from_here("../data/smoke_test/LDC93S1.wav")
-        audio_type = get_loadable_audio_type_from_extension(audio_path.suffix)
+        audio_type = audio.get_loadable_audio_type_from_extension(audio_path.suffix)
         result = audio.read_duration(audio_type, audio_path)
         self.assertEqual(isinstance(result, float))
 
