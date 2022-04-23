203367addfdcefdbce4237c6cd26d38ef175eabd
wasertech <danny@waser.tech>
2022-04-23 18:42:25 +0200

    fix without limit

--- training/coqui_stt_training/train.py
+++ training/coqui_stt_training/train.py
@@ -324,14 +324,14 @@ def create_training_datasets(
 
 
 def train():
-    #log_info("Performing dummy training to check for memory problems.")
-    #log_info(
-    #    "If the following process crashes, you likely have batch sizes "
-    #    "that are too big for your available system memory (or GPU memory)."
-    #)
-    #train_impl(epochs=1, reverse=True, limit=Config.train_batch_size * 3, write=False)
+    log_info("Performing dummy training to check for memory problems.")
+    log_info(
+        "If the following process crashes, you likely have batch sizes "
+        "that are too big for your available system memory (or GPU memory)."
+    )
+    train_impl(epochs=1, reverse=True, limit=0, write=False) #Config.train_batch_size * 3, write=False)
 
-    #log_info("Dummy run finished without problems, now starting real training process.")
+    log_info("Dummy run finished without problems, now starting real training process.")
     train_impl(epochs=Config.epochs) #, silent_load=True)
 
 
