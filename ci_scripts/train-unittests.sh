35b457f61f659feea68700f5bddc42c036393fc5
loganhart420 <loganhartpersonal@gmail.com>
2022-06-06 18:20:37 -0400

    Add libopusfile0 to lint.yml

--- ci_scripts/train-unittests.sh
+++ ci_scripts/train-unittests.sh
@@ -5,7 +5,6 @@ source $(dirname "$0")/all-vars.sh
 source $(dirname "$0")/all-utils.sh
 
 set -o pipefail
-apt-get install libopusfile0 -y
 pip install --upgrade pip setuptools wheel | cat
 pip install --upgrade . | cat
 set +o pipefail
