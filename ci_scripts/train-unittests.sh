43729517b45568eb725f2d0769177bfc5e43a6f9
loganhart420 <loganhartpersonal@gmail.com>
2022-06-06 18:02:34 -0400

    Add install to workflow

--- ci_scripts/train-unittests.sh
+++ ci_scripts/train-unittests.sh
@@ -5,7 +5,7 @@ source $(dirname "$0")/all-vars.sh
 source $(dirname "$0")/all-utils.sh
 
 set -o pipefail
-apt install libopusfile0
+apt-get install libopusfile0 -y
 pip install --upgrade pip setuptools wheel | cat
 pip install --upgrade . | cat
 set +o pipefail
