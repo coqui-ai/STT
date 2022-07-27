6353667b5979c637aa7e8d00417f7ff9637ee95a
Anton Yaroshenko <kpakadijl@hotmail.com>
2022-07-27 19:50:18 +0300

    Migrate to net framework 4.8 Migrate to netstandard2.0 and add support for net5.0, net6.0

--- native_client/dotnet/STTClient/NativeImp.cs
+++ native_client/dotnet/STTClient/NativeImp.cs
@@ -16,98 +16,98 @@ namespace STTClient
         internal static extern IntPtr STT_Version();
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal unsafe static extern ErrorCodes STT_CreateModel(string aModelPath,
+        internal static extern unsafe ErrorCodes STT_CreateModel(string aModelPath,
             ref IntPtr** pint);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal unsafe static extern IntPtr STT_ErrorCodeToErrorMessage(int aErrorCode);
+        internal static extern unsafe IntPtr STT_ErrorCodeToErrorMessage(int aErrorCode);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal unsafe static extern uint STT_GetModelBeamWidth(IntPtr** aCtx);
+        internal static extern unsafe uint STT_GetModelBeamWidth(IntPtr** aCtx);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal unsafe static extern ErrorCodes STT_SetModelBeamWidth(IntPtr** aCtx,
+        internal static extern unsafe ErrorCodes STT_SetModelBeamWidth(IntPtr** aCtx,
             uint aBeamWidth);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal unsafe static extern ErrorCodes STT_CreateModel(string aModelPath,
+        internal static extern unsafe ErrorCodes STT_CreateModel(string aModelPath,
             uint aBeamWidth,
             ref IntPtr** pint);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal unsafe static extern int STT_GetModelSampleRate(IntPtr** aCtx);
+        internal static extern unsafe int STT_GetModelSampleRate(IntPtr** aCtx);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern ErrorCodes STT_EnableExternalScorer(IntPtr** aCtx,
+        internal static extern unsafe ErrorCodes STT_EnableExternalScorer(IntPtr** aCtx,
             string aScorerPath);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern ErrorCodes STT_AddHotWord(IntPtr** aCtx,
+        internal static extern unsafe ErrorCodes STT_AddHotWord(IntPtr** aCtx,
             string aWord,
             float aBoost);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern ErrorCodes STT_EraseHotWord(IntPtr** aCtx,
+        internal static extern unsafe ErrorCodes STT_EraseHotWord(IntPtr** aCtx,
             string aWord);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern ErrorCodes STT_ClearHotWords(IntPtr** aCtx);
+        internal static extern unsafe ErrorCodes STT_ClearHotWords(IntPtr** aCtx);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern ErrorCodes STT_DisableExternalScorer(IntPtr** aCtx);
+        internal static extern unsafe ErrorCodes STT_DisableExternalScorer(IntPtr** aCtx);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern ErrorCodes STT_SetScorerAlphaBeta(IntPtr** aCtx,
+        internal static extern unsafe ErrorCodes STT_SetScorerAlphaBeta(IntPtr** aCtx,
             float aAlpha,
             float aBeta);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl,
             CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static unsafe extern IntPtr STT_SpeechToText(IntPtr** aCtx,
+        internal static extern unsafe IntPtr STT_SpeechToText(IntPtr** aCtx,
             short[] aBuffer,
             uint aBufferSize);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-        internal static unsafe extern IntPtr STT_SpeechToTextWithMetadata(IntPtr** aCtx,
+        internal static extern unsafe IntPtr STT_SpeechToTextWithMetadata(IntPtr** aCtx,
             short[] aBuffer,
             uint aBufferSize,
             uint aNumResults);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern void STT_FreeModel(IntPtr** aCtx);
+        internal static extern unsafe void STT_FreeModel(IntPtr** aCtx);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern ErrorCodes STT_CreateStream(IntPtr** aCtx,
+        internal static extern unsafe ErrorCodes STT_CreateStream(IntPtr** aCtx,
                ref IntPtr** retval);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern void STT_FreeStream(IntPtr** aSctx);
+        internal static extern unsafe void STT_FreeStream(IntPtr** aSctx);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern void STT_FreeMetadata(IntPtr metadata);
+        internal static extern unsafe void STT_FreeMetadata(IntPtr metadata);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern void STT_FreeString(IntPtr str);
+        internal static extern unsafe void STT_FreeString(IntPtr str);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl,
             CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static unsafe extern void STT_FeedAudioContent(IntPtr** aSctx,
+        internal static extern unsafe void STT_FeedAudioContent(IntPtr** aSctx,
             short[] aBuffer,
             uint aBufferSize);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern IntPtr STT_IntermediateDecode(IntPtr** aSctx);
+        internal static extern unsafe IntPtr STT_IntermediateDecode(IntPtr** aSctx);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern IntPtr STT_IntermediateDecodeWithMetadata(IntPtr** aSctx,
+        internal static extern unsafe IntPtr STT_IntermediateDecodeWithMetadata(IntPtr** aSctx,
             uint aNumResults);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl,
             CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static unsafe extern IntPtr STT_FinishStream(IntPtr** aSctx);
+        internal static extern unsafe IntPtr STT_FinishStream(IntPtr** aSctx);
 
         [DllImport("libstt.so", CallingConvention = CallingConvention.Cdecl)]
-        internal static unsafe extern IntPtr STT_FinishStreamWithMetadata(IntPtr** aSctx,
+        internal static extern unsafe IntPtr STT_FinishStreamWithMetadata(IntPtr** aSctx,
             uint aNumResults);
         #endregion
     }
